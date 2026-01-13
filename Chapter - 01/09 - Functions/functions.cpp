9bdb7f1ed427484a210f3cde80a4524a251d8451
fatma zayrek <fzayrek@gmail.com>
2026-01-13 09:01:06 +0300

    Chapter-01

---
+++ Chapter - 01/09 - Functions/functions.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "fmt/include/fmt/core.h"
+
+using namespace std;
+
+void myFunction(int i, char c);
+
+void myFunction(int i, char c)
+{
+	cout << fmt::format("the value of i is {}", i) << endl;
+	cout << fmt::format("the value of c is {}", c) << endl;
+}
+
+// int addNumbers(int number1, int number2)
+// {
+// 	cout << fmt::format("Entering function {}", __func__) << endl;
+// 	return number1 + number2;
+// }
+
+// With function return type deduction
+auto addNumbers(int number1, int number2)
+{
+	cout << fmt::format("Entering function {}", __func__) << endl;
+	return number1 + number2;
+}
+
+double addNumbers(double a, double b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int someInt{ 6 };
+	char someChar{ 'c' };
+	myFunction(8, 'a');
+	myFunction(someInt, 'b');
+	myFunction(5, someChar);
+
+	int sum{ addNumbers(5, 3) };
+
+	cout << addNumbers(1, 2) << endl; // Calls the integer version
+	cout << addNumbers(1.11, 2.22);   // Calls the double version
+}
